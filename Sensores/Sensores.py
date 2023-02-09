b644e4776a2f4bc1537a981d2312e1ea75246ef0
Israel Mantoan Júnior <israelmantoan@gmail.com>
2023-02-09 14:44:30 -0300

    Update Sensores.py

--- Sensores/Sensores.py
+++ Sensores/Sensores.py
@@ -4,7 +4,17 @@ import time # Importa a biblioteca time
 while True: # Cria um loop infinito
     cpu_percent = psutil.cpu_percent() # Obtém o uso da CPU em porcentagem
     virtual_memory = psutil.virtual_memory() # Obtém informações sobre a memória virtual
-    print("Uso da CPU:", cpu_percent, "%") # Exibe o uso da CPU
-    print("Uso da memória:", virtual_memory.used / (1024 * 1024), "MB") # Exibe o uso da memória em MB
-    time.sleep(1) # Aguarda 1 segundo antes de repetir o loop
+    disk_usage = psutil.disk_usage("/") # Obtém informações sobre o uso do disco
+    temperature = psutil.sensors_temperatures()['coretemp'][0].current # Obtém a temperatura da CPU
 
+    print("CPU Usage:", cpu_percent, "%") # Exibe informações sobre o uso da CPU
+
+    print("Memory Usage:", virtual_memory.used / (1024 * 1024), "MB") # Exibe informações sobre o uso da memória
+
+    print("Disk Usage:", disk_usage.percent, "%") # Exibe informações sobre o uso do disco
+
+    print("CPU Temperature:", temperature, "°C") # Exibe a temperatura da CPU
+
+    print("<-------------------------------->") # Separador entre cada checagem
+
+    time.sleep(3) # Aguarda 3 segundos antes de repetir o loop
